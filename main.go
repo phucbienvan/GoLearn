1bb2729065930d58b9c07fcec895a6a11f56e576
Biện Phúc <bienvanphuc98@gmail.com>
2021-06-12 15:19:50 +0700

    convert type

--- main.go
+++ main.go
@@ -2,25 +2,31 @@ package main
 
 import "fmt"
 
-var (text string = "xin chao"
-	number int = 10
+var (
+	text   string = "xin chao"
+	number int    = 10
+
 )
-func main()  {
-	m := 2
-	fmt.Println( m);
-	k := 10
-	fmt.Println(k)
-	a := k * m
-	fmt.Println("k * m = ", a)
-	fmt.Printf("%v, %T", a, a)
+
+func main() {
+	var numberFirst int = 10
+	fmt.Println(numberFirst)
+
+	var second float32 = 11.1
+	fmt.Printf("%v %T", second, second)
 	fmt.Println()
-	fmt.Println(text)
-	sum:= 0;
-	for i := 0; i < 10; i ++{
+
+	// Ep kieu
+	var numberThird int = int(second)
+	fmt.Printf("%v, %T", numberThird, numberThird)
+	fmt.Println()
+	sum := 0
+	for i := 0; i < 10; i++ {
 		sum += i
 	}
 
 	fmt.Println(sum)
+
 	
 
 }
