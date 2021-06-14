d3c54080e8a30916cbd4f44b255a9ec8f428314f
Phúc Biện <bienvanphuc98@gmail.com>
2021-06-14 15:19:22 +0700

    slice

--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"fmt"
 	"math"
-	"time"
+	// "time"
+	"sort"
 )
 
 
@@ -26,30 +27,74 @@ func main() {
 	// 	fmt.Printf("Sum = %v \n", sum)
 	// 	fmt.Println()
 	// }
-	fmt.Println(pow(5, 4, 1000))
-	t := time.Now()
-	switch {
-	case t.Hour() +10 < 12:
-		fmt.Println("Good morning!")
-	case t.Hour() < 17:
-		fmt.Println("Good afternoon.")
-	default:
-		fmt.Println("Good evening.")
-	}
-	sum := 1
-	for sum < 1000{
-		sum += sum
-	}
-	fmt.Println(sum)
-	
-	
+	// fmt.Println(pow(5, 4, 1000))
+	// t := time.Now()
+	// switch {
+	// case t.Hour() +10 < 12:
+	// 	fmt.Println("Good morning!")
+	// case t.Hour() < 17:
+	// 	fmt.Println("Good afternoon.")
+	// default:
+	// 	fmt.Println("Good evening.")
+	// }
+	// sum := 1
+	// for sum < 1000{
+	// 	sum += sum
+	// }
+	// fmt.Println(sum)
+
+	// numPointer := 9
+	// convert(numPointer)
+	// fmt.Println(numPointer)
+	// convertAddress(&numPointer)
+	// fmt.Println(numPointer)
+
+	//Slices
+	// numSlice := []int{1, 2, 10}
+	// fmt.Println(numSlice)
+	// numSlice[2] = 11
+	// fmt.Println(numSlice)
+	// fmt.Println(numSlice[0])
+
+	var ele rune
+    var size int
+    var sli = make([]int,0,1)
+    size = cap(sli)
+    for i:=0; i<=size; i++{
+        if i>=len(sli){
+            size=size+1
+        }
+        ele = 0
+        fmt.Println("Enter a number to add: ")
+        fmt.Scan(&ele)
+        if ele==0 {
+            fmt.Println("Stopping!")
+            break
+        }
+        sli = append(sli, int(ele))
+    }
+    fmt.Println(sli)
+	bubbleSoft(sli)
+	fmt.Println(sli)
 }
 
-func convert(num1 int, num2 int){
-	var tmp int = num1
-	num1 = num2
-	num2 = tmp
-	fmt.Println("new", num1, num2)
+func bubbleSoft(slide sort.IntSlice){
+	for j := 0; j < len(slide)-1; j++ {
+		for i := len(slide)-2; i >= j; i-- {
+			if slide[i] > slide[i+1] {
+				tg := slide[i]
+				slide[i] = slide[i+1]
+				slide[i+1] = tg
+			}
+		}
+	}
+}
+
+func convert(a int){
+	a = 100
+}
+func convertAddress(a *int){
+	*a = 200
 }
 
 func pow(x, n, lim float64) float64 {
