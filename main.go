15c0a9f5a7e6560d733ff541254f7f70a9762c10
Phúc Biện <bienvanphuc98@gmail.com>
2021-06-14 16:26:18 +0700

    Goroutines

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"math"
 	// "time"
+
+	"time"
 	"sort"
 )
 type person struct {
@@ -60,26 +62,26 @@ func main() {
 	// fmt.Println(numSlice)
 	// fmt.Println(numSlice[0])
 
-	var ele rune
-    var size int
-    var sli = make([]int,0,1)
-    size = cap(sli)
-    for i:=0; i<=size; i++{
-        if i>=len(sli){
-            size=size+1
-        }
-        ele = 0
-        fmt.Println("Enter a number to add: ")
-        fmt.Scan(&ele)
-        if ele==0 {
-            fmt.Println("Stopping!")
-            break
-        }
-        sli = append(sli, int(ele))
-    }
-    fmt.Println(sli)
-	bubbleSoft(sli)
-	fmt.Println(sli)
+	// var ele rune
+    // var size int
+    // var sli = make([]int,0,1)
+    // size = cap(sli)
+    // for i:=0; i<=size; i++{
+    //     if i>=len(sli){
+    //         size=size+1
+    //     }
+    //     ele = 0
+    //     fmt.Println("Enter a number to add: ")
+    //     fmt.Scan(&ele)
+    //     if ele==0 {
+    //         fmt.Println("Stopping!")
+    //         break
+    //     }
+    //     sli = append(sli, int(ele))
+    // }
+    // fmt.Println(sli)
+	// bubbleSoft(sli)
+	// fmt.Println(sli)
 
 	// struct
 	var p person
@@ -89,6 +91,22 @@ func main() {
 	fmt.Println(p)
 	newPerson := person{"Ha", "Hung",9}
 	fmt.Println(newPerson)
+
+	//goroutines
+	go go1()
+	go go2()
+	time.Sleep(time.Second)
+}
+
+func go1(){
+	for i := 0 ; i < 10; i ++ {
+		go fmt.Println(i)
+	}
+}
+func go2(){
+	for i := 10; i <20; i++ {
+		go fmt.Println(i)
+	}
 }
 
 func bubbleSoft(slide sort.IntSlice){
